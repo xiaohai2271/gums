529b991b3bc7b49979fa4b32f71065409f8708e4
禾几海 <a@celess.cn>
2022-01-20 17:31:27 +0800

    mybatis plus

--- dums-core/src/test/java/MybatisGenerator.java
+++ dums-core/src/test/java/MybatisGenerator.java
@@ -1,3 +1,12 @@
+import cn.celess.dums.DumsApplication;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>date: 2022/01/20</P>
  * <p>desc: </p>
@@ -6,7 +15,33 @@
  * @author 禾几海
  */
 public class MybatisGenerator {
+    public static final String URL = "jdbc:postgresql://localhost:5432/dums";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "zhenghai";
+    public static final String[] TABLE_NAME = {};
+
     public static void main(String[] args) {
 
+        String projectPath = System.getProperty("user.dir");
+        Map<OutputFile, String> outputFileStringMap = new HashMap<>();
+        outputFileStringMap.put(OutputFile.mapperXml, projectPath + "/dums-core/src/main/resources/mapper");
+        outputFileStringMap.put(OutputFile.entity, projectPath + "dums-model/src/main/java");
+
+        FastAutoGenerator.create(URL, USERNAME, PASSWORD)
+                .globalConfig(builder -> {
+                    builder.author("禾几海") // 设置作者
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir(projectPath + "/dums-core/src/main/java"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("cn.celess.dums") // 设置父包名
+                            .pathInfo(outputFileStringMap); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude(TABLE_NAME) // 设置需要生成的表名
+                            .addTablePrefix("dums_"); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
     }
 }
